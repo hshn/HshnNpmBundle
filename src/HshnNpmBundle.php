eab421a14976038bf0f243f002759cb4c473c270
Shota Hoshino <sht.hshn@gmail.com>
2016-02-07 00:36:02 +0900

    Added bundle

---
+++ src/HshnNpmBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hshn\NpmBundle\HshnNpmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Shota Hoshino <sht.hshn@gmail.com>
+ */
+class HshnNpmBundle extends Bundle
+{
+
+}
